27290f5de8d25a7a4717661f7d59ee59ca77aab1
Tilen Faganel <tilen.faganel@me.com>
2014-11-01 11:59:32 +0100

    Updated code styles

--- app/src/main/java/course/lab/modernartui/MainActivity.java
+++ app/src/main/java/course/lab/modernartui/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends Activity {
 
     @Override
@@ -14,7 +13,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
