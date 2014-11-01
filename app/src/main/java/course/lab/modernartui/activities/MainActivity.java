5f815e35cc2c4a35f9dd575765406fc4ef3f7092
Tilen Faganel <tilen.faganel@me.com>
2014-11-02 00:45:55 +0100

    Added colored rectangles

--- app/src/main/java/course/lab/modernartui/activities/MainActivity.java
+++ app/src/main/java/course/lab/modernartui/activities/MainActivity.java
@@ -1,11 +1,14 @@
-package course.lab.modernartui;
+package course.lab.modernartui.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import course.lab.modernartui.R;
 import course.lab.modernartui.dialogs.MoreInformationDialog;
 
 /**
@@ -15,11 +18,24 @@ public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getName();
 
+    private RelativeLayout palette;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        palette = (RelativeLayout) findViewById(R.id.palette);
+
+        for (int i = 0; i < 10; i++) {
+
+            View v = new View(this);
+
+            v.setLayoutParams(new ViewGroup.LayoutParams(100, 100));
+            v.setBackgroundColor(R.color.lightgray);
+            palette.addView(v);
+        }
     }
 
     @Override
