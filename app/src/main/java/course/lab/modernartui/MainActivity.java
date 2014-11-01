0b552a8d754a2ba211a2a3a1a67c1a7172b8fe00
Tilen Faganel <tilen.faganel@me.com>
2014-11-01 11:57:16 +0100

    Updated gitignore with android studio files

--- app/src/main/java/course/lab/modernartui/MainActivity.java
+++ app/src/main/java/course/lab/modernartui/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -17,7 +18,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
