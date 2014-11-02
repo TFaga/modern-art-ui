5e218bc3a22d63670c1ab45d609ab0a445e717e9
Tilen Faganel <tilen.faganel@me.com>
2014-11-02 10:43:17 +0100

    Added color changing mechanism

--- app/src/main/java/course/lab/modernartui/activities/MainActivity.java
+++ app/src/main/java/course/lab/modernartui/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package course.lab.modernartui.activities;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
@@ -43,6 +45,30 @@ public class MainActivity extends Activity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
+                for (int i = 0; i < palette.getChildCount(); i++) {
+                    View child = palette.getChildAt(i);
+
+                    int originalColor = Color.parseColor((String) child.getTag());
+                    int invertedColor = (0x00FFFFFF - (originalColor | 0xFF000000)) |
+                            (originalColor & 0xFF000000);
+
+                    if (getResources().getColor(R.color.white) != originalColor && getResources()
+                            .getColor(R.color.lightgray) != originalColor) {
+
+                        int origR = (originalColor >> 16) & 0x000000FF;
+                        int origG = (originalColor >> 8) & 0x000000FF;
+                        int origB = originalColor & 0x000000FF;
+
+                        int invR = (invertedColor >> 16) & 0x000000FF;
+                        int invG = (invertedColor >> 8) & 0x000000FF;
+                        int invB = invertedColor & 0x000000FF;
+
+                        child.setBackgroundColor(Color.rgb((int) (origR + (invR - origR) *
+                                (progress / 100f)), (int) (origG + (invG - origG) * (progress /
+                                100f)), (int) (origB + (invB - origB) * (progress / 100f))));
+                        child.invalidate();
+                    }
+                }
             }
 
             /**
