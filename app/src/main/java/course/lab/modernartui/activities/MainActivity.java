990d9493452c3a170e43de5e2ee289b16e5e0b82
Tilen Faganel <tilen.faganel@me.com>
2014-11-02 01:12:23 +0100

    Removed unused code

--- app/src/main/java/course/lab/modernartui/activities/MainActivity.java
+++ app/src/main/java/course/lab/modernartui/activities/MainActivity.java
@@ -4,9 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 
 import course.lab.modernartui.R;
 import course.lab.modernartui.dialogs.MoreInformationDialog;
@@ -19,6 +18,7 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getName();
 
     private RelativeLayout palette;
+    private SeekBar seek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +27,46 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         palette = (RelativeLayout) findViewById(R.id.palette);
+        seek = (SeekBar) findViewById(R.id.seekBar);
 
-        for (int i = 0; i < 10; i++) {
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
-            View v = new View(this);
+            /**
+             * Notification that the progress level has changed. Clients can use the fromUser
+             * parameter
+             * to distinguish user-initiated changes from those that occurred programmatically.
+             *
+             * @param seekBar  The SeekBar whose progress has changed
+             * @param progress The current progress level. This will be in the range 0..100
+             * @param fromUser True if the progress change was initiated by the user.
+             */
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-            v.setLayoutParams(new ViewGroup.LayoutParams(100, 100));
-            v.setBackgroundColor(R.color.lightgray);
-            palette.addView(v);
-        }
+            }
+
+            /**
+             * Notification that the user has started a touch gesture. Clients may want to use this
+             * to disable advancing the seekbar.
+             *
+             * @param seekBar The SeekBar in which the touch gesture began
+             */
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            /**
+             * Notification that the user has finished a touch gesture. Clients may want to use this
+             * to re-enable advancing the seekbar.
+             *
+             * @param seekBar The SeekBar in which the touch gesture began
+             */
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
